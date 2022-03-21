80da627595f85cc5d6eabb4a749841d2f62814df
Robert Sopko <robert.sopko@slovenskoit.sk>
2022-03-21 12:30:42 +0000

    WEB-416 Zakladna kostra projektu

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
+import dts from "rollup-plugin-dts";
+
+const packageJson = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({
+        exclude: [
+          // Exclude test files
+          /\.test.((js|jsx|ts|tsx))$/,
+          // Exclude story files
+          /\.stories.((js|jsx|ts|tsx|mdx))$/,
+        ],
+        tsconfig: "./tsconfig.json"
+      }),
+      copy({
+        targets: [
+          { src: 'src/styles/idsk3_theme.css', dest: 'dist/styles' },
+        ]
+      })
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
---
+++ src/components/Atoms/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface ButtonProps {
+  label: string;
+}
+
+const Button = (props: ButtonProps) => {
+  return <button
+    className='inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+  >
+    {props.label}
+  </button>;
+};
+
+export default Button;
