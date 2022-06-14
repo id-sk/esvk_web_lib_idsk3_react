6d305b4b04b0302775cea921e5284070231aa910
Ivan Kačala <ivan.kacala@slovenskoit.sk>
2022-06-14 15:03:43 +0200

    WEB-713 footerButton classNames

--- src/components/Atoms/ModalSideBar/ModalSideBarFooterButton.tsx
+++ src/components/Atoms/ModalSideBar/ModalSideBarFooterButton.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import { ArrowForwardIcon } from '../../../svgIcons/Navigation';
 
 const ModalSideBarFooterButton = React.forwardRef<
   HTMLAnchorElement,
   React.AnchorHTMLAttributes<HTMLAnchorElement>
->(({ href, onClick, children, ...props }, ref) => {
-  const elementProps = {
-    className: 'modal-sidebar__footer-button',
-    ...props
-  };
+>(({ href, onClick, children, className, ...props }, ref) => {
   return (
-    <a href={href} onClick={onClick} {...elementProps} ref={ref}>
+    <a
+      ref={ref}
+      href={href}
+      onClick={onClick}
+      className={classNames('modal-sidebar__footer-button', className)}
+      {...props}
+    >
       {children} <ArrowForwardIcon className="modal-sidebar__forward-icon" />
     </a>
   );
