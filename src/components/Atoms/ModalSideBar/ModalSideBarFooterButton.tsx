a59c44748f90514af0f18cf5d6815693b52003e2
Ivan Kacala <ivan.kacala@slovenskoit.sk>
2022-06-14 10:03:31 +0200

    WEB-713 footerButton forward ref

--- src/components/Atoms/ModalSideBar/ModalSideBarFooterButton.tsx
+++ src/components/Atoms/ModalSideBar/ModalSideBarFooterButton.tsx
@@ -9,26 +9,22 @@ export interface ModalSideBarFooterButtonProps {
   children?: ReactNode | undefined;
 }
 
-const ModalSideBarFooterButton = ({
-  href,
-  onClick,
-  children,
-  htmlTag = 'a',
-  ...props
-}: ModalSideBarFooterButtonProps) => {
-  const elementProps = {
-    className: 'modal-sidebar__footer-button',
-    ...props
-  };
-  return htmlTag === 'a' ? (
-    <a href={href ? href : undefined} {...elementProps}>
-      {children} <ArrowForwardIcon className="modal-sidebar__forward-icon" />
-    </a>
-  ) : (
-    <button onClick={!!onClick ? onClick : undefined} {...elementProps}>
-      {children} <ArrowForwardIcon className="modal-sidebar__forward-icon" />
-    </button>
-  );
-};
+const ModalSideBarFooterButton = React.forwardRef<HTMLAnchorElement, ModalSideBarFooterButtonProps>(
+  ({ href, onClick, children, htmlTag = 'a', ...props }, ref) => {
+    const elementProps = {
+      className: 'modal-sidebar__footer-button',
+      ...props
+    };
+    return htmlTag === 'a' ? (
+      <a href={href ? href : undefined} {...elementProps} ref={ref}>
+        {children} <ArrowForwardIcon className="modal-sidebar__forward-icon" />
+      </a>
+    ) : (
+      <button onClick={!!onClick ? onClick : undefined} {...elementProps}>
+        {children} <ArrowForwardIcon className="modal-sidebar__forward-icon" />
+      </button>
+    );
+  }
+);
 
 export default ModalSideBarFooterButton;
