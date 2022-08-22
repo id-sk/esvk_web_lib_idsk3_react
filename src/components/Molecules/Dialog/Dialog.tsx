cf5b07ad614911e4dc9296514c5e333ce9b84563
Amália Hudáková <amalia.hudakova@slovenskoit.sk>
2022-08-22 18:12:57 +0200

    WEB1-119 safari bug

--- src/components/Molecules/Dialog/Dialog.tsx
+++ src/components/Molecules/Dialog/Dialog.tsx
@@ -20,25 +20,27 @@ const Dialog = ({ opened, toggleOpened, id, ...props }: DialogProps) => {
 
   return (
     <div className={dialogClasses}>
-      <div className="dialog">
-        <div className="dialog__header">
-          <div className="dialog__header-title">
-            <h2>{props.title}</h2>
-            <button
-              onClick={toggleOpened}
-              className="dialog__close-icon"
-              id={id ? id + '-dialog-close-button' : undefined}
-            >
-              <CloseIcon />
-            </button>
+      <div className="dialog-wrapper">
+        <div className="dialog">
+          <div className="dialog__header">
+            <div className="dialog__header-title">
+              <h2>{props.title}</h2>
+              <button
+                onClick={toggleOpened}
+                className="dialog__close-icon"
+                id={id ? id + '-dialog-close-button' : undefined}
+              >
+                <CloseIcon />
+              </button>
+            </div>
+            {!!props.description && <p className="subtitle mt-4">{props.description}</p>}
           </div>
-          {!!props.description && <p className="subtitle mt-4">{props.description}</p>}
-        </div>
 
-        <div className="flex gap-2 flex-col flex-start px-7 py-5">{props.children}</div>
-        <div className="dialog__buttons">
-          {props.secondaryButton}
-          {props.primaryButton}
+          <div className="flex gap-2 flex-col flex-start px-7 py-5">{props.children}</div>
+          <div className="dialog__buttons">
+            {props.secondaryButton}
+            {props.primaryButton}
+          </div>
         </div>
       </div>
     </div>
