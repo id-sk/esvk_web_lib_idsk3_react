9e60bbbc4e0998ba692a477d491ff6961702f143
Tibor Dulovec <tibor.dulovec@slovenskoit.sk>
2022-09-19 17:37:26 +0200

    WEB1-242: Feedback - added story without report button

--- src/stories/Feedback.stories.tsx
+++ src/stories/Feedback.stories.tsx
@@ -15,6 +15,12 @@ const Template: ComponentStory<typeof Feedback> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   yesButton: 'Áno',
+  noButton: 'Nie'
+};
+
+export const WithReportButton = Template.bind({});
+WithReportButton.args = {
+  yesButton: 'Áno',
   noButton: 'Nie',
   reportButton: 'Nahlásiť chybu'
 };
