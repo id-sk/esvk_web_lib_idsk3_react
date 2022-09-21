35373f6e7258b6069ed498dd399ccca2440a0b58
Maros Vizina <maros.vizina@slovenskoit.sk>
2022-09-21 16:30:17 +0200

    WEB2-168 loader spinner custom classname

--- src/components/Atoms/Loader/Loader.tsx
+++ src/components/Atoms/Loader/Loader.tsx
@@ -1,14 +1,18 @@
+import classNames from 'classnames';
 import React from 'react';
 import { SpinnerIcon } from '../../../svgIcons/Animated';
 
 export interface LoaderProps extends React.AllHTMLAttributes<HTMLDivElement> {
   label?: string;
+  spinnerClassName?: string;
 }
 
-const Loader = ({ label, ...props }: LoaderProps) => {
+const Loader = ({ label, spinnerClassName, ...props }: LoaderProps) => {
+  const spinnerClass = classNames('loader__icon', spinnerClassName);
+
   return (
     <div className="loader" {...props}>
-      <SpinnerIcon className="loader__icon" />
+      <SpinnerIcon className={spinnerClass} />
       {label && <span className="loader__label">{label}</span>}
     </div>
   );
