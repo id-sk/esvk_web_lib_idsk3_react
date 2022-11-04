2ec5a3d41801107005bcd31ba5797fc639a00a7f
Amália Hudáková <amalia.hudakova@slovenskoit.sk>
2022-11-04 10:48:51 +0100

    WEB1-402

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -18,6 +18,49 @@ Default.args = {
   headRow: (
     <>
       <TableRowValue>Názov</TableRowValue>
+      <TableRowValue>Podpísané</TableRowValue>
+      <TableRowValue>Podpis</TableRowValue>
+      <TableRowValue align="right">Akcie</TableRowValue>
+    </>
+  ),
+  children: (
+    <>
+      <TableRow>
+        <TableRowValue>Martin Mucha</TableRowValue>
+        <TableRowValue>19.1.2022 o 15:32</TableRowValue>
+        <TableRowValue>
+          <a href="#" className="link-m font-bold">
+            Detail podpisu
+          </a>
+        </TableRowValue>
+        <TableRowValue align="right">
+          <button>
+            <MoreVertIcon className="w-6 h-6" />
+          </button>
+        </TableRowValue>
+      </TableRow>
+      <TableRow>
+        <TableRowValue>Martin Mucha</TableRowValue>
+        <TableRowValue>19.1.2022 o 15:32</TableRowValue>
+        <TableRowValue>
+          <a href="#" className="link-m font-bold">
+            Detail podpisu
+          </a>
+        </TableRowValue>
+        <TableRowValue align="right">
+          <button>
+            <MoreVertIcon className="w-6 h-6" />
+          </button>
+        </TableRowValue>
+      </TableRow>
+    </>
+  )
+};
+export const Attachments = Template.bind({});
+Attachments.args = {
+  headRow: (
+    <>
+      <TableRowValue>Názov</TableRowValue>
       <TableRowValue>Pridané</TableRowValue>
       <TableRowValue>Podpis</TableRowValue>
       <TableRowValue align="right">Akcie</TableRowValue>
