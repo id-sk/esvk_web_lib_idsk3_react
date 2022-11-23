1f535fa441264bd29c182b3a676027931bfdf415
TenTibor <tibqor@gmail.com>
2022-11-23 13:21:55 +0100

    WEB1-462: Edited format of size

--- src/utils/formatBytes.ts
+++ src/utils/formatBytes.ts
@@ -1,12 +1,12 @@
 const formatBytes = (bytes: number, decimals = 0): string => {
-  if (bytes === 0) return '0B';
+  if (bytes === 0) return '0 b';
 
   const k = 1024;
   const dm = decimals < 0 ? 0 : decimals;
-  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const sizes = ['B', 'kB', 'mB', 'gB', 'tB', 'pB', 'eB', 'zB', 'yB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
 
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + sizes[i];
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 };
 
 export default formatBytes;
