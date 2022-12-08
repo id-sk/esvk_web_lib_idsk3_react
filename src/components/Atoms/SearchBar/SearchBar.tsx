105ae84a2536d8b258e307eaef2e8f73c573ff04
TenTibor <tibqor@gmail.com>
2022-12-08 13:49:47 +0100

    WEB1-554: SearchBar - Added support for references

--- src/components/Atoms/SearchBar/SearchBar.tsx
+++ src/components/Atoms/SearchBar/SearchBar.tsx
@@ -18,91 +18,96 @@ export interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputEleme
   label?: string;
 }
 
-const SearchBar = ({
-  buttonLabel,
-  buttonAriaLabel,
-  buttonDisabled,
-  placeholder,
-  searchbarSize = 'large',
-  openable = false,
-  fullWidth = false,
-  containerClassName,
-  wrapperClassName,
-  className,
-  buttonOnClick,
-  id,
-  style,
-  error,
-  errorMsg,
-  label,
-  ...props
-}: SearchBarProps) => {
-  const [searchbarOpened, setSearchbarOpened] = useState<boolean>(false);
-
-  const inputClasses: string = classNames(
-    'searchbar',
+const SearchBar = React.forwardRef<HTMLInputElement, SearchBarProps>(
+  (
     {
-      'searchbar--large': searchbarSize === 'large',
-      'searchbar--medium': searchbarSize === 'medium',
-      'searchbar--small': searchbarSize === 'small',
-      'searchbar--w-full': fullWidth
-    },
-    className
-  );
-  const buttonClasses: string = classNames('searchbar__button', {
-    'searchbar__button--large': searchbarSize === 'large',
-    'searchbar__button--medium': searchbarSize === 'medium',
-    'searchbar__button--small': searchbarSize === 'small'
-  });
-  const iconClasses: string = classNames({
-    'searchbar__icon--large': searchbarSize === 'large',
-    'searchbar__icon--medium': searchbarSize === 'medium',
-    'searchbar__icon--small': searchbarSize === 'small'
-  });
-  const contentClasses = classNames(
-    {
-      relative: fullWidth,
-      searchbar__wrapper: !fullWidth,
-      hidden: openable && !searchbarOpened
-    },
-    wrapperClassName
-  );
-  const buttonRef = useRef<HTMLButtonElement>(null);
+      buttonLabel,
+      buttonAriaLabel,
+      buttonDisabled,
+      placeholder,
+      searchbarSize = 'large',
+      openable = false,
+      fullWidth = false,
+      containerClassName,
+      wrapperClassName,
+      className,
+      buttonOnClick,
+      id,
+      style,
+      error,
+      errorMsg,
+      label,
+      ...props
+    }: SearchBarProps,
+    ref
+  ) => {
+    const [searchbarOpened, setSearchbarOpened] = useState<boolean>(false);
 
-  return (
-    <div className={containerClassName}>
-      {openable && (
-        <IconLink
-          children={<SearchIcon />}
-          onClick={() => setSearchbarOpened((p) => !p)}
-          className={searchbarOpened ? 'hidden' : ''}
-        />
-      )}
-      <div className={contentClasses}>
-        <label className="sr-only" htmlFor={id ? id + '-input' : undefined}>
-          {label}
-        </label>
-        <input
-          className={inputClasses}
-          placeholder={placeholder}
-          id={id ? id + '-input' : undefined}
-          {...props}
-        />
-        <button
-          onClick={buttonOnClick}
-          className={buttonClasses}
-          disabled={buttonDisabled}
-          ref={buttonRef}
-          id={id ? id + '-button' : undefined}
-          aria-label={buttonAriaLabel}
-        >
-          <SearchIcon className={iconClasses} />
-          {buttonLabel}
-        </button>
+    const inputClasses: string = classNames(
+      'searchbar',
+      {
+        'searchbar--large': searchbarSize === 'large',
+        'searchbar--medium': searchbarSize === 'medium',
+        'searchbar--small': searchbarSize === 'small',
+        'searchbar--w-full': fullWidth
+      },
+      className
+    );
+    const buttonClasses: string = classNames('searchbar__button', {
+      'searchbar__button--large': searchbarSize === 'large',
+      'searchbar__button--medium': searchbarSize === 'medium',
+      'searchbar__button--small': searchbarSize === 'small'
+    });
+    const iconClasses: string = classNames({
+      'searchbar__icon--large': searchbarSize === 'large',
+      'searchbar__icon--medium': searchbarSize === 'medium',
+      'searchbar__icon--small': searchbarSize === 'small'
+    });
+    const contentClasses = classNames(
+      {
+        relative: fullWidth,
+        searchbar__wrapper: !fullWidth,
+        hidden: openable && !searchbarOpened
+      },
+      wrapperClassName
+    );
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    return (
+      <div className={containerClassName}>
+        {openable && (
+          <IconLink
+            children={<SearchIcon />}
+            onClick={() => setSearchbarOpened((p) => !p)}
+            className={searchbarOpened ? 'hidden' : ''}
+          />
+        )}
+        <div className={contentClasses}>
+          <label className="sr-only" htmlFor={id ? id + '-input' : undefined}>
+            {label}
+          </label>
+          <input
+            className={inputClasses}
+            placeholder={placeholder}
+            id={id ? id + '-input' : undefined}
+            {...props}
+          />
+          <button
+            onClick={buttonOnClick}
+            className={buttonClasses}
+            disabled={buttonDisabled}
+            ref={buttonRef}
+            id={id ? id + '-button' : undefined}
+            aria-label={buttonAriaLabel}
+          >
+            <SearchIcon className={iconClasses} />
+            {buttonLabel}
+          </button>
+        </div>
+        {error && !!errorMsg && <p className="input__caption--error">{errorMsg}</p>}
       </div>
-      {error && !!errorMsg && <p className="input__caption--error">{errorMsg}</p>}
-    </div>
-  );
-};
+    );
+  }
+);
 
 export default SearchBar;
