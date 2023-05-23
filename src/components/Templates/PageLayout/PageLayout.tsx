44df85a9308af668984588d7ad568924654eebe1
Ivan Kačala <ivan.kacala@slovenskoit.sk>
2023-05-23 16:24:47 +0200

    WEB2-1710 hero image

--- src/components/Templates/PageLayout/PageLayout.tsx
+++ src/components/Templates/PageLayout/PageLayout.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useLayoutEffect } from 'react';
 import classNames from 'classnames';
 
 export interface PageLayoutProps extends React.AllHTMLAttributes<HTMLElement> {
@@ -8,6 +8,7 @@ export interface PageLayoutProps extends React.AllHTMLAttributes<HTMLElement> {
   breadcrumbs?: ReactNode;
   contentClassName?: string;
   informationBanner?: ReactNode;
+  heroImage?: ReactNode;
 }
 
 const PageLayout = ({
@@ -19,15 +20,46 @@ const PageLayout = ({
   footer,
   className,
   contentClassName,
+  heroImage = <></>,
   ...props
 }: PageLayoutProps) => {
+  const headingRef = React.useRef<HTMLDivElement>(null);
+  const mainRef = React.useRef<HTMLDivElement>(null);
+
+  const [heroHeight, setHeroHeight] = React.useState(0);
+
+  useLayoutEffect(() => {
+    const calculateHeroDimensions = () => {
+      const bottomLineComponent = headingRef.current?.getBoundingClientRect();
+      const topLineComponent = mainRef.current?.getBoundingClientRect();
+      setHeroHeight((bottomLineComponent?.bottom ?? 0) - (topLineComponent?.top ?? 0));
+    };
+    calculateHeroDimensions();
+    window.addEventListener('resize', calculateHeroDimensions);
+    return () => window.removeEventListener('resize', calculateHeroDimensions);
+  }, []);
+
   return (
     <div className="idsk-page-layout">
       {!!informationBanner && <div>{informationBanner}</div>}
       {header}
-      <main className={classNames('idsk-page-layout__main', className)} {...props}>
+      <main className={classNames('idsk-page-layout__main', className)} ref={mainRef} {...props}>
+        {!!heroImage && (
+          <div className="relative h-0 top-[-1.875rem]">
+            <div
+              className="w-full h-8 absolute inset-x-0 top-0 overflow-hidden"
+              style={{ height: `${heroHeight}px` }}
+            >
+              {heroImage}
+            </div>
+          </div>
+        )}
         {!!breadcrumbs && <div className="idsk-page-layout__breadcrumbs">{breadcrumbs}</div>}
-        {!!heading && <div className="idsk-page-layout__heading">{heading}</div>}
+        {!!heading && (
+          <div className="idsk-page-layout__heading" ref={headingRef}>
+            {heading}
+          </div>
+        )}
         <div className={classNames('idsk-page-layout__content', contentClassName)}>{children}</div>
       </main>
       {footer}
