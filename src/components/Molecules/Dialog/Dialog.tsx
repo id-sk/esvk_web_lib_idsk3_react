625388e12e90700ea65d82e1241f17e822c905a4
Maros Vizina <maros.vizina@slovenskoit.sk>
2023-06-08 12:50:21 +0200

    Dialog fix

--- src/components/Molecules/Dialog/Dialog.tsx
+++ src/components/Molecules/Dialog/Dialog.tsx
@@ -2,8 +2,10 @@ import React, { ReactNode } from 'react';
 import FocusLock from 'react-focus-lock';
 import { CloseIcon } from '../../../svgIcons/Navigation';
 import classNames from 'classnames';
+import { ReactFocusLockProps } from 'react-focus-lock/interfaces';
 
-export interface DialogProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface DialogProps extends ReactFocusLockProps {
+  id?: string;
   title?: string;
   description?: string;
   opened?: boolean;
@@ -35,39 +37,37 @@ const Dialog = ({
   );
 
   return (
-    <div {...props} className={dialogClasses} id={id}>
-      <FocusLock>
-        <div className="idsk-dialog-wrapper">
-          <div className="idsk-dialog">
-            {!!title && (
-              <div className="idsk-dialog__header">
-                <div className="idsk-dialog__header-title">
-                  <h2>{title}</h2>
-                  <button
-                    onClick={toggleOpened}
-                    className="idsk-dialog__close-icon"
-                    id={id ? id + '-dialog-close-button' : undefined}
-                    aria-label={closeButtonAriaLabel}
-                  >
-                    <CloseIcon />
-                  </button>
-                </div>
-                {!!description && <p className="idsk-dialog__header-description">{description}</p>}
+    <FocusLock {...props} className={dialogClasses} lockProps={{ id: id }}>
+      <div className="idsk-dialog-wrapper">
+        <div className="idsk-dialog">
+          {!!title && (
+            <div className="idsk-dialog__header">
+              <div className="idsk-dialog__header-title">
+                <h2>{title}</h2>
+                <button
+                  onClick={toggleOpened}
+                  className="idsk-dialog__close-icon"
+                  id={id ? id + '-dialog-close-button' : undefined}
+                  aria-label={closeButtonAriaLabel}
+                >
+                  <CloseIcon />
+                </button>
               </div>
-            )}
+              {!!description && <p className="idsk-dialog__header-description">{description}</p>}
+            </div>
+          )}
 
-            <div className="idsk-dialog__content">{children}</div>
+          <div className="idsk-dialog__content">{children}</div>
 
-            {!!(secondaryButton || primaryButton) && (
-              <div className="idsk-dialog__buttons">
-                {secondaryButton}
-                {primaryButton}
-              </div>
-            )}
-          </div>
+          {!!(secondaryButton || primaryButton) && (
+            <div className="idsk-dialog__buttons">
+              {secondaryButton}
+              {primaryButton}
+            </div>
+          )}
         </div>
-      </FocusLock>
-    </div>
+      </div>
+    </FocusLock>
   );
 };
 
