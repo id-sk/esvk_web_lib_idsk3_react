b2a0ce789eabcaa221cd82fd79773ba08176fd17
Maros Vizina <maros.vizina@slovenskoit.sk>
2023-11-15 15:17:29 +0100

    Tooltip browser fix

--- src/components/Atoms/Tooltip/Tooltip.tsx
+++ src/components/Atoms/Tooltip/Tooltip.tsx
@@ -16,7 +16,7 @@ const Tooltip = ({ tooltip, children, ...props }: TooltipProps) => {
     'idsk-tooltip--instructive': !!props.isInstructive
   });
   return (
-    <div role="tooltip" title={tooltip} data-tooltip={tooltip} className={tooltipClasses}>
+    <div data-tooltip={tooltip} className={tooltipClasses}>
       {children}
     </div>
   );
